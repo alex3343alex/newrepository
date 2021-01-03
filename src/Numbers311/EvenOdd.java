5a433ad52cad635229f619970a1b0918dacdcd0d
Oleh Bobko <alex3343alex@gmail.com>
2021-01-03 20:58:56 +0200

    Last changes 02.01.2021

--- src/Numbers311/EvenOdd.java
+++ src/Numbers311/EvenOdd.java
@@ -3,21 +3,22 @@ import java.io.*;
 
 public class EvenOdd {
     public static void main(String[] args) throws IOException{
+        System.out.println("Введіть якесь число");
         try{
             int n;
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
             n = Integer.parseInt(in.readLine());
             if (n % 2 == 0)
             {
-                System.out.println("Given number is Even.");
+                System.out.println("Ваше число парне");
             }
             else
             {
-                System.out.println("Given number is Odd.");
+                System.out.println("Ваше число непартне");
             }
         }
         catch(NumberFormatException e){
-            System.out.println(e.getMessage() + " is not a numeric value.");
+            System.out.println(e.getMessage() + " Дане число не є цілим");
             System.exit(0);
         }
     }
--- src/Numbers311/OddNumbersCalculator.java
+++ src/Numbers311/OddNumbersCalculator.java
@@ -1,4 +1,21 @@
 package Numbers311;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class OddNumbersCalculator {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter num a");
+            int a = s.nextInt();
+            System.out.println("Enter num b");
+            int b = s.nextInt();
+            int sum = a + b;
+            System.out.println(sum);
+        } catch (InputMismatchException e) {
+            System.out.println("You should enter integer only");
+        }
+    }
 }
